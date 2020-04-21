c3c01441f12ff8f849a3e6aee1313725aed1e1df
activedigital17 <active.digital17@gmail.com>
2020-04-21 22:17:51 +0200

    added loading/splash screen

---
+++ app/src/main/java/com/quizapp/SplashScreen.kt
@@ -0,0 +1,28 @@
+package com.quizapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+
+class SplashScreen : AppCompatActivity() {
+
+    private val SPLASH_TIME_OUT:Long=3000
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        Handler().postDelayed({
+            // This method will be executed once the timer is over
+            // Start your app main activity
+
+            startActivity(Intent(this,MainActivity::class.java))
+
+            // close this activity
+            finish()
+        }, SPLASH_TIME_OUT)
+    }
+}
